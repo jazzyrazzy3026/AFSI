74d22c05fa25942538ffd6ddd5f3b0330feaa299
jazzyrazzy3026 <vonyango2030@theaskacademy.org>
2026-06-28 09:12:10 -0600

    Add AFS Improvements user script

---
+++ tornado_mod/afsi.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         AFS Improvements(AFSI)
+// @description  Changes to make geofs better.
+// @version      1.0
+// @author       jazzyrazzy3026
+// @match        https://www.geo-fs.com/geofs.php?*
+// @connect      github.com
+// @connect      raw.githubusercontent.com
+// @downloadURL  https://raw.githubusercontent.com/jazzyrazzy3026/AFSI/main/afsi.user.js
+// @updateURL    https://raw.githubusercontent.com/jazzyrazzy3026/AFSI/main/afsi.user.js
+// @run-at       document-start
+// @grant        none
+//
+// === AUTOMATIC MODULE DOWNLOAD ===
+// @require      https://raw.githubusercontent.com/jazzyrazzy3026/AFSI/main/particle.module.js
+// =================================
+// ==/UserScript==
+
